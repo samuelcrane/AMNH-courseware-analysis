3d502f73632b74fa533d4905f04f771d83113d3c
Samuel Crane <sam@samuelcrane.com>
2013-09-27 15:42:58 -0400

    Update plot_quiz_responses.r

--- plot_quiz_responses.r
+++ plot_quiz_responses.r
@@ -3,7 +3,7 @@
 # https://github.com/samuelcrane
 #
 # R code
-# Updated: 2013-09-19
+# Updated: 2013-09-27
 # Faceted bar chart of quantitative quiz results from Coursera MOOC data
 # Format of CSV input file:
 # question,option,responses,option_text
@@ -12,9 +12,12 @@
 # ...
 
 
+library('ggplot2')
+
 # Faceted bar charts of Quiz Question responses
-quizResp <- read.csv('week1/quiz_response_table.csv')
-quizNumber <- "Quiz 1 Question Responses"
+# The input CSV file here is a cleaned version of the Quiz Summary TXT file that Coursera provides
+quizResp <- read.csv('/Week3/plot/input/quiz_response_table.csv')
+quizNumber <- "Quiz 3 Question Responses"
 qh <- ggplot(quizResp, aes(x=option, y=responses)) 
 qh <- qh + geom_bar(binwidth=.5, colour="black", fill="white", stat="identity")
 qh <- qh + facet_grid(. ~ question)
