9b05f9614cdc8a280ddfadbc0dffdc5c93812529
Samuel Crane <sam@samuelcrane.com>
2013-11-27 12:53:59 -0500

    Update count_essay_pageviews.py
    Added unique page view counts, output to CSV file, cleaned up some parts.

--- count_essay_pageviews.py
+++ count_essay_pageviews.py
@@ -2,32 +2,57 @@
 # https://github.com/samuelcrane
 
 import json
+import csv
 
-def count_pageviews(URL, essay):
+def count_total_pageviews(essay, clickstream):
     """ (str, str) -> int
 
-    Print the essay and number of pageviews of that essay by counting
-    the number of times the essay's URL appears in the pageview events
-    in the Coursrea clickstream json file. 
-
-    >>> count_pageviews(json_key, 'https://class.coursera.org/amnhgenetics-001/wiki/view?page=Genetics_Short_History')
-    https://class.coursera.org/amnhgenetics-001/wiki/view?page=Genetics_Short_History
-    VIEWS: 4269
-    >>> count_pageviews('page_url', 'https://class.coursera.org/amnhgenetics-001/wiki/view?page=Mapping_Morality')
-    https://class.coursera.org/amnhgenetics-001/wiki/view?page=Mapping_Morality
-    VIEWS: 786
+    Opens the clickstream file and parses one line at a time (each
+    line in the file is a valid JSON object and corresponds to a user
+    event), and counts up the number of pageviews if the event loaded
+    the essay URL string.
+    
+    >>> count_total_pageviews('https://class.coursera.org/amnhgenetics-001/wiki/view?page=Genetics_Short_History')
+    4269
+    >>> count_total_pageviews('https://class.coursera.org/amnhgenetics-001/wiki/view?page=Mapping_Morality')
+    786
     """
     number_of_pageviews = 0
-    with open('amnhgenetics-001_clickstream_export') as f:
+    with open(clickstream) as f:
+        print 'Counting total pageviews...'
         for line in f:
             event = json.loads(line)
-            if event[URL] == essay:
+            if event['page_url'] == essay:
                 number_of_pageviews += 1
-    print essay
-    print 'VIEWS:', number_of_pageviews
+        print 'Done.'
+    return number_of_pageviews
 
+def count_unique_pageviews(essay, clickstream):
+    """ (str, str) -> int
 
-json_key = 'page_url'
+    Opens the clickstream file and parses one line at a time (each
+    line in the file is a valid JSON object and corresponds to a user
+    event), and if the event loaded the essay URL string, adds the
+    username to a list of unique usernames. If the username is already
+    in the list (because the user has already been encountered and was
+    previously added to the list), then the event is skipped. Thus, the
+    length of the unique username list is the number of unique pageviews
+
+    >>> count_unique_pageviews('https://class.coursera.org/amnhgenetics-001/wiki/view?page=Genetics_Short_History')
+    2245
+    >>> count_unique_pageviews('https://class.coursera.org/amnhgenetics-001/wiki/view?page=Mapping_Morality')
+    565
+    """
+    unique_users = []
+    with open(clickstream) as f:
+        print 'Counting unique pageviews...'
+        for line in f:
+            event = json.loads(line)
+            if event['page_url'] == essay and event['username'] not in unique_users:
+                unique_users.append(event['username'])
+        print 'Done.'
+    unique_pageviews = len(unique_users)
+    return unique_pageviews
 
 essay_list = [
 'https://class.coursera.org/amnhgenetics-001/wiki/view?page=Genetics_Short_History',
@@ -46,5 +71,25 @@ essay_list = [
 'https://class.coursera.org/amnhgenetics-001/wiki/view?page=Mapping_Morality'
 ]
 
+# Specify the location of the JSON-serialized clickstream file provided by Coursera
+clickstream = 'amnhgenetics-001_clickstream_export'
+
+# Initialize list for CSV output and specify header names
+essay_pageviews = [['essay_title','total_views','unique_views','average_views','essay_url']]
+
+# Process all the essays
 for essay in essay_list:
-    count_pageviews(json_key, essay)
+    essay_title = essay.replace('https://class.coursera.org/amnhgenetics-001/wiki/view?page=', '').replace('_', ' ')
+    print '\nEssay:', essay_title
+    total = count_total_pageviews(essay, clickstream)
+    unique = count_unique_pageviews(essay, clickstream)
+    average_visits = format(total/float(unique), '.2f')
+    essay_pageviews.append([essay_title,total,unique,average_visits,essay])
+
+print '\nWriting output file...'
+output_file = open('essay_pageviews.csv', 'w')
+datawriter = csv.writer(output_file)
+datawriter.writerows(essay_pageviews)
+output_file.close()
+
+print 'Finished counting essay page views.'
